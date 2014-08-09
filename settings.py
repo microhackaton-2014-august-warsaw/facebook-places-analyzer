2678515aff5b79b844386f10211d4c860f736a10
Wojtek Erbetowski <wojtek@erbetowski.pl>
2014-08-09 19:27:32 +0200

    x

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ RABBITMQ_PORT = 5672
 
 SERVICE_IP = environ.get('SERVICE_IP') or '12.34.56.78'
 
-LOGGING_FILE = environ.get('LOGGING_FILE') or "/tmp/log"
+LOGGING_FILE = environ.get('LOGGING_FILE') or "/home/deployment/logs/facebook-places-analyser.log"
