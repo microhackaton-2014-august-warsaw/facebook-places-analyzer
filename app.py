c91f4760c9d2200f09d84af04bca7908259f8ccb
Dmitry Kolesnikov <don.dimas@gmail.com>
2014-08-09 15:38:53 +0200

    input json parsed and handled

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
 import logging
 import json
+import latlontool
 
 from os import environ
 import pika
+import facebook_correlator
 
 QUEUE_NAME = 'facebook_posts'
 RABBITMQ_HOST = environ.get('RABBITMQ_HOST') or 'localhost'
 RABBITMQ_PORT = environ.get('RABBITMQ_PORT') or 5672
 
 
+def place_data(location):
+    return latlontool.place_data(location["latitude"], location["longitude"])
+
+def place_with_probability(place):
+    place_extended = dict()
+    place_extended["place"] = place
+    place_extended["probability"] = "high"
+    place_extended["origin"] = "facebook"
+    return place_extended
+
 def consume_posts(ch, method, properties, body):
-    logging.info('Consuming posts...')
     try:
         data = json.loads(body)
         profileLocation = data["hometown"]["location"]
         profileHometown = data["location"]["location"]
+        places = list()
+        profileLocationCode = place_data(profileLocation)
+        places.append(place_with_probability(profileLocationCode))
+        profileHometownCode = place_data(profileHometown)
+        places.append(place_with_probability(profileHometownCode))
+        posts = data["posts"]
+        for post in posts:
+            print post["created_time"]
+            if "place" in post:
+                location = post["place"]["location"]
+                code = place_data(location)
+                places.append(place_with_probability(code))
+                print post["place"]["location"]
         locationPost0 = data["posts"][0]["place"]["location"]
+        profileLocation = data["hometown"]["location"]
+        profileHometown = data["location"]["location"]
+        locationPost0 = data["posts"][0]["place"]["location"]
+        output = dict()
+        output["places"] = places
+        facebook_correlator.post_localizations(output)
         print "Consuming posts: {}".format(data)
     except Exception, e:
         print(e)
@@ -34,6 +64,7 @@ if __name__ == '__main__':
 
     # Make sure the queue exists
     channel.queue_declare(queue=QUEUE_NAME)
+    channel.queue_declare(queue='facebook_posts')
 
     print ' [*] Waiting for messages. To exit press CTRL+C'
 
