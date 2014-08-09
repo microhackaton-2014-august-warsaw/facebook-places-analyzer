b7933c2f633954cafb423f95663bee73234c885d
Tomasz Netczuk <netczuk.tomasz@gmail.com>
2014-08-09 15:14:39 +0200

    Mock message pushes dummy message to the app.

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
-from latlontool import place_data
-from os import environ
-import pika
 import logging
-import urllib2
 import json
 
+from os import environ
+import pika
 
+QUEUE_NAME = 'facebook_posts'
 RABBITMQ_HOST = environ.get('RABBITMQ_HOST') or 'localhost'
 RABBITMQ_PORT = environ.get('RABBITMQ_PORT') or 5672
 
+
 def consume_posts(ch, method, properties, body):
-    data = json.loads(body)
-    profileLocation = data["hometown"]["location"]
-    profileHometown = data["location"]["location"]
-    locationPost0 = data["posts"][0]["place"]["location"]
-    print "Consuming posts: {}".format(data)
+    logging.info('Consuming posts...')
+    try:
+        data = json.loads(body)
+        profileLocation = data["hometown"]["location"]
+        profileHometown = data["location"]["location"]
+        locationPost0 = data["posts"][0]["place"]["location"]
+        print "Consuming posts: {}".format(data)
+    except Exception, e:
+        print(e)
+        logging.info(e)
 
 
 if __name__ == '__main__':
@@ -28,7 +33,7 @@ if __name__ == '__main__':
     channel = connection.channel()
 
     # Make sure the queue exists
-    channel.queue_declare(queue='facebook_posts')
+    channel.queue_declare(queue=QUEUE_NAME)
 
     print ' [*] Waiting for messages. To exit press CTRL+C'
 
--- mock_message.py
+++ mock_message.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+from os import environ
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+QUEUE_NAME = 'facebook_posts'
+RABBITMQ_HOST = environ.get('RABBITMQ_HOST') or 'localhost'
+RABBITMQ_PORT = environ.get('RABBITMQ_PORT') or 5672
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue=QUEUE_NAME)
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
+channel.basic_publish(exchange='', routing_key=('%s' % QUEUE_NAME), body='Hello World!')
 
 print " [x] Sent 'Hello World!'"
 
